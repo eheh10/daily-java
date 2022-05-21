e61ebb90a5c3879ba2970e6d7c6f775af9e348d1
eunhye park <lime1pm@naver.com>
2022-05-21 23:36:12 +0900

    Feat: Fix execute method parameter in Operation

--- src/main/java/com/operation/Operation.java
+++ src/main/java/com/operation/Operation.java
@@ -2,8 +2,9 @@ package com.operation;
 
 import com.tag.Tag;
 
+import java.util.Map;
 import java.util.Queue;
 
 public interface Operation {
-    void execute(Queue<Tag> tags, Queue<Tag> executableTags);
+    void execute(Queue<Tag> tags, Queue<Tag> executableTags, Map<Tag,Object> executeFailTags);
 }
