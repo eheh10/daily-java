aa95b470d015a13005cfd913f3b8aac50d0f6bc9
eunhye park <lime1pm@naver.com>
2022-05-21 21:46:40 +0900

    Feat: ADD OperationFactory

---
+++ src/main/java/com/operation/OperationFactory.java
@@ -0,0 +1,17 @@
+package com.operation;
+
+import java.util.Objects;
+
+public class OperationFactory {
+    public Operation create(String cmd){
+        String[] metas = cmd.split(" ");
+
+        if (Objects.equals(metas[0],"execute")) {
+            return new ExecuteOperation(metas[1]);
+        }else if(Objects.equals(metas[0],"create")){
+            return new CreateOperation();
+        }
+
+        throw new RuntimeException();
+    }
+}
