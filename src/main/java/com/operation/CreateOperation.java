5876234ebfbaea22f46376289e19db9e816f4640
eunhye park <lime1pm@naver.com>
2022-05-21 23:36:37 +0900

    Feat: Fix execute method in CreateOperation

--- src/main/java/com/operation/CreateOperation.java
+++ src/main/java/com/operation/CreateOperation.java
@@ -1,21 +1,22 @@
 package com.operation;
 
-import com.main.Main;
 import com.tag.Tag;
 
-import java.util.Objects;
+import java.util.Map;
+import java.util.Queue;
 
 public class CreateOperation implements Operation{
 
     @Override
-    public void execute() {
-        for(Tag t: Main.tags){
-            if(t.isReady()){
-                t.create();
-                return;
-            }
+    public void execute(Queue<Tag> tags, Queue<Tag> executableTags, Map<Tag,Object> executeFailTags) {
+        boolean isCreated = false;
+        Tag t = tags.poll();
+        if(t!=null) {
+            isCreated = executableTags.offer(t);
         }
 
-        Main.createFail++;
+        if(!isCreated){
+            Tag.createFailCount();
+        }
     }
 }
