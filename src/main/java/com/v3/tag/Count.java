74c0dffc59427e4aca5aacc7016bbeff01c802cb
eunhye park <lime1pm@naver.com>
2022-06-01 11:30:38 +0900

    Feat: ADD Count class of v3

---
+++ src/main/java/com/v3/tag/Count.java
@@ -0,0 +1,13 @@
+package com.v3.tag;
+
+public class Count {
+    private int cnt = 0;
+
+    public void Counting(){
+        cnt++;
+    }
+
+    public int getCount(){
+        return cnt;
+    }
+}
