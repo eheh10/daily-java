7a12814f95df31a20186d85ed21732d53e82ed85
eunhye park <lime1pm@naver.com>
2022-06-10 23:27:37 +0900

    Feat: ADD Count of v4

---
+++ src/main/java/com/v4/count/Count.java
@@ -0,0 +1,13 @@
+package com.v4.count;
+
+public class Count {
+    private int cnt = 0;
+
+    public void increase(){
+        cnt++;
+    }
+
+    public String getCount(){
+        return cnt+"";
+    }
+}
