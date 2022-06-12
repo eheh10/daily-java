b6437cac5c5f10adb79109ce07ed8337256e87d2
eunhye park <lime1pm@naver.com>
2022-06-12 19:12:33 +0900

    Refactor: Fix Integer comparison in TagComparator

--- src/main/java/com/v4/tag/TagComparator.java
+++ src/main/java/com/v4/tag/TagComparator.java
@@ -2,11 +2,12 @@ package com.v4.tag;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Objects;
 
 public class TagComparator implements Comparator<Map.Entry<Tag,Integer>> {
     @Override
     public int compare(Map.Entry<Tag, Integer> o1, Map.Entry<Tag, Integer> o2) {
-        if(o1.getValue() != o2.getValue()){
+        if(Objects.equals(o1.getValue(),o2.getValue())){ //Integer는 비교연산자 x
             return o2.getValue() - o1.getValue();
         }
         return Integer.parseInt(o1.getKey().toString()) - Integer.parseInt(o2.getKey().toString());
