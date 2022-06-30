7ab500fa6f246f7dd2accb64bd1910126cf084aa
eunhye park <lime1pm@naver.com>
2022-06-30 23:34:40 +0900

    Fix Test3_V2

--- src/main/java/test/Test3_V2.java
+++ src/main/java/test/Test3_V2.java
@@ -1,8 +1,56 @@
 package test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test3_V2 {
+    static class Word{
+
+        private final List<Character> values;
+
+        public Word(List<Character> values){
+            this.values = values;
+        }
+
+        public static Word from(String value){ //사용처 편의 제공
+            List<Character> values = new ArrayList<>();
+            for(char c:value.toCharArray()){
+                values.add(c);
+            }
+
+            return new Word(values);
+        }
+
+        public Word reverseConverter() {
+            List<Character> reverseValues = new ArrayList<>();
+
+            for(int i=0; i<values.size(); i++) {
+                reverseValues.add(searchReverseCharacter(values.get(i)));
+            }
+
+            return new Word(reverseValues);
+        }
+
+        private Character searchReverseCharacter(Character c) {
+            if (97 <= c && c <= 122) {
+                return (char) (122 - c + 97);
+            }
+            if (65 <= c && c <= 90) {
+                return (char) (90 - c + 65);
+            }
+            return c;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder str = new StringBuilder();
+            for(char c:values){
+                str.append(c);
+            }
+            return str.toString();
+        }
+    }
     public static void main(String[] args) throws IOException {
         InputStream is = System.in;
         BufferedInputStream bis = new BufferedInputStream(is,8192);
@@ -13,21 +61,9 @@ public class Test3_V2 {
     }
 
     public static String solution(String word) {
-        StringBuilder answer = new StringBuilder();
+        Word originalWord = Word.from(word);
+        Word reverseWord = originalWord.reverseConverter();
 
-        for(char c:word.toCharArray()){
-            answer.append(reverseConverter(c));
-        }
-
-        return answer.toString();
-    }
-
-    private static char reverseConverter(char c) {
-        if( 97 <= c && c <= 122 ){
-            return (char)(122-c+97);
-        }else if( 65 <= c && c <= 90 ) {
-            return (char) (90 - c + 65);
-        }
-        return c;
+        return reverseWord.toString();
     }
 }
