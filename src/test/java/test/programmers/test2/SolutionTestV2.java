2105461dccc40be0f666ea7e3fdac4ebc5ffb0f3
eunhye park <lime1pm@naver.com>
2022-07-14 13:49:16 +0900

    Fix Test2
    인형뽑기 리팩토링

---
+++ src/test/java/test/programmers/test2/SolutionTestV2.java
@@ -0,0 +1,72 @@
+package test.programmers.test2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTestV2 {
+    static class NumberSet{
+        private final List<Integer> values;
+
+        public NumberSet(List<Integer> values) {
+            this.values = values;
+        }
+
+        public static NumberSet from(int size){
+            List<Integer> values = new ArrayList<>(Math.max(10,size));
+            return new NumberSet(values);
+        }
+
+        public void add(int num){
+            if (values.size() == 0) {
+                values.add(num);
+                return;
+            }
+
+            if (values.get(values.size()-1) == num){
+                remove(values.size()-1);
+                return;
+            }
+
+            values.add(num);
+        }
+
+        private void remove(int index){
+            values.remove(index);
+        }
+
+        public int size(){
+            return values.size();
+        }
+
+    }
+
+    @DisplayName("입출력으로 주어진 예시를 통과하는지 테스트 합니다.")
+    @ParameterizedTest
+    @ArgumentsSource(SolutionTestSuiteArgumentsProvider.class)
+    void testExample(int[][] board, int[] moves, int expectAnswer) {
+        NumberSet numberSet = NumberSet.from(board.length);
+
+        int basket = 0;
+        for(int num:moves){
+            for(int i=0; i<board.length; i++){
+                if (board[i][num-1] != 0){
+                    numberSet.add(board[i][num-1]);
+
+                    board[i][num-1] = 0;
+                    basket++;
+                    break;
+                }
+            }
+        }
+
+        int answer = basket - numberSet.size();
+
+        assertThat(answer).isEqualTo(expectAnswer);
+    }
+}
