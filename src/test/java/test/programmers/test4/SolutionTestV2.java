293f61d687f97cc7bf4b1b730c1618e3b7ccc222
eunhye park <lime1pm@naver.com>
2022-07-22 11:53:50 +0900

    FIX cache (null check in City class)

--- src/test/java/test/programmers/test4/SolutionTestV2.java
+++ src/test/java/test/programmers/test4/SolutionTestV2.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
