094fe67f7748893b2b21396cdb08d67cf75095ba
eunhye park <lime1pm@naver.com>
2022-07-27 22:17:19 +0900

    Fix tomato solving with DFS

---
+++ src/test/java/test/algorithm/tomato/SolutionTestWithDFS.java
@@ -0,0 +1,135 @@
+package test.algorithm.tomato;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SolutionTestWithDFS {
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+    static Deque<Node> bucket = new ArrayDeque<>();
+
+    static class Node{
+        private final int row;
+        private final int col;
+        private final List<int[]> path;
+
+        public Node(int row, int col, List<int[]> path) {
+            this.row = row;
+            this.col = col;
+
+            List<int[]> copy = new ArrayList<>();
+            for(int[] p : path){
+                copy.add(new int[]{p[0],p[1]});
+            }
+            this.path = copy;
+        }
+
+        public boolean isInvalid(int maxRow, int maxColumn){
+            return !(0 <= row && row <= maxRow && 0 <= col && col <= maxColumn);
+        }
+
+        public Node updatePath(){
+            path.add(new int[]{row, col});
+            return new Node(row,col,path);
+        }
+
+        public Node scalNode(int scalRow, int scalCol) {
+            return new Node(row+scalRow, col+scalCol, path);
+        }
+
+    }
+
+    @DisplayName("입출력으로 주어진 예시를 통과하는지 테스트 합니다.")
+    @ParameterizedTest
+    @ArgumentsSource(SolutionTestSuiteArgumentsProvider.class)
+    void testExample(int M, int N, int[][]box, int expectAnswer) {
+        int answer = -1;
+        bucket = new ArrayDeque<>();
+
+        for(int row=N-1; 0 <= row ; row--){
+            for(int col=M-1; 0 <= col; col--){
+
+                if(box[row][col] != 1) {
+                    continue;
+                }
+
+                Node node = new Node(row,col, new ArrayList<>());
+                bucket.push(node);
+            }
+        }
+
+        while(!bucket.isEmpty()) {
+            Node node = bucket.pop();
+
+//            System.out.println("current: "+node.row+","+node.col);
+//            for(int[] path:node.path){
+//                System.out.print(Arrays.toString(path)+"->");
+//            }
+//            System.out.println();
+
+//            display(bucket);
+
+            if (isAllRipe(box)){
+                answer = node.path.size();
+                break;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Node nextNode = node.scalNode(dy[i],dx[i]);
+
+                if(nextNode.isInvalid(N-1, M-1)) {
+                    continue;
+                }
+
+                if(alreadyRipe(box,nextNode)){
+                    continue;
+                }
+
+                if(notExist(box,nextNode)){
+                    continue;
+                }
+
+//                System.out.println("next: "+nextNode.row+","+nextNode.col);
+                ripe(box, nextNode.updatePath());
+            }
+        }
+        assertThat(answer).isEqualTo(expectAnswer);
+    }
+
+    private void display(Deque<Node> bucket) {
+        Iterator<Node> it = bucket.iterator();
+
+        while(it.hasNext()){
+            Node n = it.next();
+            System.out.println(n.row+","+n.col);
+        }
+    }
+
+    private boolean isAllRipe(int[][] arr) {
+        for(int[] row : arr){
+            for(int n : row){
+                if (n==0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void ripe(int[][] arr, Node node) {
+        arr[node.row][node.col] = 1;
+        bucket.push(node);
+    }
+    private boolean notExist(int[][] arr, Node node) {
+        return arr[node.row][node.col] == -1;
+    }
+
+    public boolean alreadyRipe(int[][] arr, Node node) {
+        return arr[node.row][node.col] == 1;
+    }
+}
